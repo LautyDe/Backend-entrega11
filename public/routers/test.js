c8f5fe74fc36d22851d0ac850f9286b9055651d1
Lautaro Demonte <lauty.d.p@gmail.com>
2022-12-23 16:43:07 -0300

    guardo la primera parte de la consigna ya terminada (incluir 5 productos de test usando faker)

--- public/routers/test.js
+++ public/routers/test.js
@@ -1,8 +1,19 @@
 const { Router } = require("express");
 const router = Router();
+const faker = require("faker");
+faker.locale = "es";
 
 router.get("/", (req, res) => {
-    res.send({ TAMO: "ACTIVO" });
+    const randomProduct = [];
+    for (let index = 0; index < 5; index++) {
+        randomProduct.push({
+            id: index + 1,
+            title: faker.commerce.product(),
+            price: faker.commerce.price(),
+            thumbnail: faker.image.imageUrl(),
+        });
+    }
+    res.json(randomProduct);
 });
 
 module.exports = router;
