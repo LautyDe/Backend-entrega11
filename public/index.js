50651da510f2dc70a3c65c1797882d4aadf46cd1
Lautaro Demonte <lauty.d.p@gmail.com>
2022-12-29 17:48:00 -0300

    agrego al formulario del chat y a la function addMessage() la adaptacion al nuevo formato de mensaje

--- public/index.js
+++ public/index.js
@@ -4,9 +4,10 @@ function MessagesRender(data) {
     const html = data
         .map(elem => {
             return `<div>
-        <strong style='color: blue'>${elem.email}</strong>
-        [<span style='color: brown'>${elem.date}</span>]:
+        <strong style='color: blue'>${elem.author.email}</strong>
+        <span style='color: brown'>${elem.date}</span>:
         <i style='color: green'>${elem.text}</i>
+        <img class='img' src=${elem.author.avatar} alt='imagen de avatar'>
         </div>`;
         })
         .join(" ");
@@ -19,13 +20,43 @@ socket.on("message", data => {
 
 function addMessage(e) {
     const message = {
-        email: document.getElementById("email").value,
+        author: {
+            id: document.getElementById("email").value,
+            name: document.getElementById("name").value,
+            lastName: document.getElementById("lastName").value,
+            age: document.getElementById("age").value,
+            aka: document.getElementById("aka").value,
+            avatar: document.getElementById("avatar").value,
+        },
         text: document.getElementById("text").value,
     };
-    if (!message.email) {
+
+    if (!message.author.id) {
         alert(
             "Por favor, introduzca un email para mandar un mensaje en el chat"
         );
+    } else if (!message.author.name) {
+        alert(
+            "Por favor, introduzca un nombre para mandar un mensaje en el chat"
+        );
+    } else if (!message.author.lastName) {
+        alert(
+            "Por favor, introduzca un apellido para mandar un mensaje en el chat"
+        );
+    } else if (!message.author.age) {
+        alert(
+            "Por favor, introduzca una edad para mandar un mensaje en el chat"
+        );
+    } else if (!message.author.aka) {
+        alert(
+            "Por favor, introduzca un alias para mandar un mensaje en el chat"
+        );
+    } else if (!message.author.avatar) {
+        alert(
+            "Por favor, introduzca un link para su avatar para mandar un mensaje en el chat"
+        );
+    } else if (!message.text) {
+        alert("Por favor, introduzca un mensaje para enviar");
     } else {
         socket.emit("new-message", message);
     }
